48139c4729a560ae130dd32524fbe258f48aafa0
Lightning <aleksarseniie@gmail.com>
2024-12-12 22:12:30 +0300

    Update Words.dpr

--- Words/Words.dpr
+++ Words/Words.dpr
@@ -3,24 +3,45 @@ program Words;
 uses SysUtils;
 
 var
-  S: string;
-  PlayerCount: integer;
-  Scores: array [1 .. 4] of integer;
+  S, Source: string;
+  PlayerCount, WordCount: integer;
+  Score, CurrScore: array [1 .. 4] of integer;
+  UsedWords: array of string;
 
 const
   alphabet =
     'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЬьЫыЭэЮюЯя';
 
-function GetScore(S, Source: string): integer;
+function GetScore(S, Source: string; var UsedWords: array of string;
+  var WordCount: integer): integer;
+var
+  Flag: boolean;
+  i: integer;
 begin
-  result := 0;
-  for var i := 1 to Length(S) do
-    if Pos(S[i], Source) <> 0 then
-      Delete(Source, Pos(S[i], Source), 1)
-    else
-      Dec(result);
-  if result >= 0 then
-    result := Length(S);
+  i := 0;
+  Flag := true;
+  repeat
+    if UsedWords[i] = S then
+      Flag := false;
+    Inc(i);
+  until (not Flag) or (i = WordCount);
+  if Flag then
+  begin
+    result := 0;
+    for i := 1 to Length(S) do
+      if Pos(S[i], Source) <> 0 then
+        Delete(Source, Pos(S[i], Source), 1)
+      else
+        Dec(result);
+    if result = 0 then
+    begin
+      result := Length(S);
+      UsedWords[WordCount] := S;
+      Inc(WordCount);
+    end;
+  end
+  else
+    result := -Length(S);
 
 end;
 
@@ -35,12 +56,12 @@ begin
   end
   else
   begin
-    for var index := 1 to length(S) do
+    for var index := 1 to Length(S) do
     begin
-      if pos(S[index], alphabet) > 0 then
-        inc(counter);
+      if Pos(S[index], alphabet) > 0 then
+        Inc(counter);
     end;
-    if counter = length(S) then
+    if counter = Length(S) then
       result := true
     else
     begin
@@ -58,15 +79,26 @@ begin
     end;
   until PlayerCount in [2 .. 4];
   repeat
+    Write('Введите исходную строку: ');
+    readLn(Source);
+  until (checking_correct(Source));
+  SetLength(UsedWords, PlayerCount * Length(Source));
+  UsedWords[0] := Source;
+  WordCount := 1;
+  repeat
     for var i := 1 to PlayerCount do
     begin
-      Readln(S);
-      S:=checking_correct(S);
-      Writeln(S);
-
-
+      Write('Слово игрока ', i, ': ');
+      readLn(S);
+      if checking_correct(S) then
+      begin
+        CurrScore[i] := GetScore(S, Source, UsedWords, WordCount);
+        Score[i] := Score[i] + CurrScore[i];
+        Writeln('Игрок ', i, ' получает ', CurrScore[i], ' очков');
+      end;
+      Writeln('У ', i, ' игрока ', Score[i], ' очков');
     end;
-  until true;
+  until (CurrScore[1]=0);
   readLn;
 
 // Change
