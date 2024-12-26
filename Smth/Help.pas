49a83300ad0bfae841e3d48a4a44e0abb8b66767
Lightning <aleksarseniie@gmail.com>
2024-12-26 23:24:57 +0300

    Марго, твой ход

--- Smth/Help.pas
+++ Smth/Help.pas
@@ -5,13 +5,13 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation;
+  FMX.Controls.Presentation, FMX.Objects;
 
 type
   TFrame3 = class(TFrame)
     Label1: TLabel;
     Button2: TButton;
-    Label2: TLabel;
+    Text1: TText;
   private
     { Private declarations }
   public
